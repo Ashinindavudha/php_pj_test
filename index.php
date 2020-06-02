f6da78160c8f757230d718b7690cca292aaa05d8
AshinIndavudha <ashinindavudha2018@gmail.com>
2020-06-02 10:24:38 +0630

    First Commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PHP TODO PROJECT</title>
+	<?php include ('head.php'); ?>
+</head>
+<body>
+<h1 class="bg-info text-white justify-content-center text-center mx-auto p-3">PHP ToDo Project</h1>
+<div class="class container">
+	<div class="class row justify-content-center">
+		<form action="" method="POST">
+		<div class="class input-group">
+			<input type="text" class="form-control">
+			<span class="ml-5"><button type="submit" class="btn btn-primary">Save</button></span>
+		</div>
+		</form>
+	</div>
+	<div class="row justify-content-center mt-3">
+		<ul class="list-group">
+			<li class="list-group-item">go to university <a href="" class="btn btn-warning mt-3 ml-5">Edit</a><a href="" class="btn btn-danger mt-3 ml-3">X</a> </li>
+			<li class="list-group-item">go to university <a href="" class="btn btn-warning mt-3 ml-5">Edit</a><a href="" class="btn btn-danger mt-3 ml-3">X</a> </li>
+
+			<li class="list-group-item">go to university <a href="" class="btn btn-warning mt-3 ml-5">Edit</a><a href="" class="btn btn-danger mt-3 ml-3">X</a> </li>
+			<li class="list-group-item">go to university <a href="" class="btn btn-warning mt-3 ml-5">Edit</a><a href="" class="btn btn-danger mt-3 ml-3">X</a> </li>
+			<li class="list-group-item">go to university <a href="" class="btn btn-warning mt-3 ml-5">Edit</a><a href="" class="btn btn-danger mt-3 ml-3">X</a> </li>
+			<li class="list-group-item">go to Shopping <a href="" class="btn btn-warning mt-3 ml-5">Edit</a><a href="" class="btn btn-danger mt-3 ml-3">X</a> </li>
+			<li class="list-group-item">go to Pagoda <a href="" class="btn btn-warning mt-3 ml-5">Edit</a><a href="" class="btn btn-danger mt-3 ml-3">X</a> </li>
+		</ul>
+	</div> 
+	</div>
+</div>
+</body>
+</html>
